38cd84273d702109d663c4b16a9c4ae78ed2d3a5
André Bergner <andre.bergner.0@gmail.com>
2016-10-27 09:29:09 +0200

    Added missing pop_back_iterator header.

---
+++ neuronal_net/pop_back_iterator.h
@@ -0,0 +1,43 @@
+# include <boost/iterator/iterator_facade.hpp>
+
+namespace iter
+{
+
+   template <typename Container>
+   class pop_back_iterator : public boost::iterator_facade
+                             <  pop_back_iterator<Container>
+                             ,  typename Container::value_type
+                             ,  boost::forward_traversal_tag
+                             >
+   {
+   public:
+
+      explicit pop_back_iterator(Container& c) : cont_(c) {}
+
+      pop_back_iterator(pop_back_iterator const&) = delete;
+      pop_back_iterator(pop_back_iterator&&) = default;
+
+   private:
+      friend class boost::iterator_core_access;
+
+      bool equal(pop_back_iterator const& that) const
+      {
+         return &cont_ == &that.cont_;
+      }
+
+      void increment() { cont_.pop_back(); }
+
+      typename Container::value_type& dereference() const { return cont_.back(); }
+
+      Container&  cont_;
+   };
+
+
+   template <typename Container>
+   auto  back_remover( Container& c )
+   {
+      return pop_back_iterator<Container>(c);
+   }
+
+
+}
