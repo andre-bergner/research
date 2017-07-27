0791e98f128acc5555065937cf19ac28f3c39f0c
André Bergner <andre.bergner.0@gmail.com>
2017-07-27 23:00:02 +0200

    Fixing performance issue in tensorflow version of hello gradient.

--- neuronal_net/learn_tensorflow/hello_gradient.py
+++ neuronal_net/learn_tensorflow/hello_gradient.py
@@ -5,17 +5,18 @@ import timer
 x = tf.placeholder(tf.float32)
 y = tf.placeholder(tf.float32)
 a = tf.Variable([1.], tf.float32)
+l = a*x
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
 
-dist_inout = 0.5*(y-a*x)*(y-a*x)
+dist_inout = 0.5*(y-l)**2
 step = tf.train.GradientDescentOptimizer(0.01).minimize(dist_inout)
 
 def one_step():
    sess.run(step, {x:1.,y:0.})
-   return sess.run(a*x, {x:1.})[0]
+   return sess.run(l, {x:1.})[0]
 
 with timer.Timer() as t:
    result = [ one_step() for n in range(1000) ]
