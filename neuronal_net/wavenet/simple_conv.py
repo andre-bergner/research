fca201b2e8319dda69c3883f4408a6f724ec38b0
André Bergner <andre.bergner.0@gmail.com>
2017-08-09 11:11:03 +0200

    Wavenet: Dilations vs strides.

--- neuronal_net/wavenet/simple_conv.py
+++ neuronal_net/wavenet/simple_conv.py
@@ -16,15 +16,25 @@ model_f = B.function([model.input],[model.output])
 model_f([ np.ones((1,4)) ])
 """
 
+model1 = M.Sequential()
+model1.add(L.Reshape((4,1), input_shape=(4,)))
+model1.add(L.Conv1D(1, kernel_size=(2), strides=(2), weights=[np.ones((2,1,1)), np.zeros(1)]))
+model1.add(L.Conv1D(1, kernel_size=(2), strides=(2), weights=[np.ones((2,1,1)), np.zeros(1)]))
+model1.add(L.Flatten())
 
-model = M.Sequential()
-model.add(L.Reshape((4,1), input_shape=(4,)))
-model.add(L.Conv1D(1, kernel_size=(2), strides=(2), weights=[np.ones((2,1,1)), np.zeros(1)]))
-model.add(L.Conv1D(1, kernel_size=(2), strides=(2), weights=[np.ones((2,1,1)), np.zeros(1)]))
-model.add(L.Flatten())
-#model.add(L.Activation('sigmoid'))
+model1_f = B.function([model1.input],[model1.layers[1].output,model1.layers[2].output,model1.output])
+print("Using Strides")
+print(model1_f([ np.matrix([1,2,3,4]) ]))
 
-model_f = B.function([model.input],[model.layers[1].output,model.layers[2].output,model.output])
-print(model_f([ np.matrix([1,2,3,4]) ]))
+
+model2 = M.Sequential()
+model2.add(L.Reshape((4,1), input_shape=(4,)))
+model2.add(L.Conv1D(1, kernel_size=(2), dilation_rate=(1), weights=[np.ones((2,1,1)), np.zeros(1)]))
+model2.add(L.Conv1D(1, kernel_size=(2), dilation_rate=(2), weights=[np.ones((2,1,1)), np.zeros(1)]))
+model2.add(L.Flatten())
+
+model2_f = B.function([model2.input],[model2.layers[1].output,model2.layers[2].output,model2.output])
+print("Using Dilations")
+print(model2_f([ np.matrix([1,2,3,4]) ]))
 
 
