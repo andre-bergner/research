a3bd4e96d730749fdbcc39d2b54c046ee04f97e9
André Bergner <andre.bergner.0@gmail.com>
2017-08-28 19:10:26 +0200

    Refactored construction of dyadic grid into parametrizable function.

--- neuronal_net/wavenet/wavelet_haar.py
+++ neuronal_net/wavenet/wavelet_haar.py
@@ -23,28 +23,43 @@ def make_haar_kernel(kernel):
 def make_lo(): return make_haar_kernel([1,1])
 def make_hi(): return make_haar_kernel([1,-1])
 
-# create operator nodes
-input = L.Input(shape=(4,))
-reshape = L.Reshape((4,1))
-hi1 = make_hi()
-lo1 = make_lo()
-hi2 = make_hi()
-lo2 = make_lo()
-flatten = L.Flatten()
+def build_dyadic_grid(num_levels = 3):
 
-# connect noes to graph
-in3d = reshape(input)
-h1 = hi1(in3d)
-l1 = lo1(in3d)
-h2 = hi2(l1)
-l2 = lo2(l1)
-#model_out = L.concatenate([L.Flatten()(h1),L.Flatten()(h2),L.Flatten()(l2)])
-model_out = L.concatenate([h1,h2,l2],axis=1)
-# current output is in pyramids
-# → h1 needs to be tranposed before flatten, options (both not working) .T or B.transpose()
+   # --- create all operators (nodes) ----------------------
+   input_len = 2**num_levels
+   input = L.Input(shape=(input_len,))
+   reshape = L.Reshape((input_len,1))
+   his = []
+   los = []
+   for _ in range(num_levels):
+      his.append(make_hi())
+      los.append(make_lo())
+   flatten = L.Flatten()
+
+   # --- connect nodes to graph ----------------------------
+   out_layers = []
+   current_level_in = reshape(input)
+   for hi,lo in zip(his,los):
+      h = hi(current_level_in)
+      l = lo(current_level_in)
+      current_level_in = l
+      out_layers.append(h)
+   out_layers.append(l)
+
+   model_out = L.concatenate(out_layers, axis=1)
+   # current output is in pyramids (stack of layers)
+   # → h1 needs to be tranposed before flatten, options (both not working) .T or B.transpose()
+
+   observables = [hi.output for hi in his]
+   observables.append(los[-1].output)
+   observables.append(model_out)
+
+   model_f = B.function([input], observables)
+
+   return model_f
 
 
-model1_f = B.function([input], [hi1.output, hi2.output, lo2.output, model_out])
+model1_f = build_dyadic_grid(2)
 print("Using Strides")
 
 hi1_o, hi2_o, lo2_o, net_o = model1_f([ np.matrix([1,2,3,4,3,2,1,0]) ])
