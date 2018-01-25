b3e1fdc57f57791aa99f67a59cb6642f036f8300
André Bergner <andre.bergner.0@gmail.com>
2018-01-25 21:32:21 +0100

    timeshift_autoencoder: cleanup, more notes, DAE.

--- neuronal_net/autoencoder/timeshift_autoencoder.py
+++ neuronal_net/autoencoder/timeshift_autoencoder.py
@@ -3,6 +3,14 @@
 # ✔ concatenate with x-fade
 # ✔ try different shifts
 # ✔ try different frame_size's
+# • second-order prediction --> train model and model^2 with frame[+1] and frame[+2], respectively
+# • DAE
+# • DAE in second order prediction?
+# • contracting AE
+# • use distance between prediction and original as stopping/quality metric
+# • test with Rössler & Lorenz signal
+# • "average" over several succesfully learned models --> common structure?
+# • try to add wavelet-ae outer ring
 # • try loss functions: fft, separate lopass & hipass filters
 # • try adding noise (denoising timeshift AE)
 # • try to increase frame_size using deep-conv with downsampling
@@ -14,10 +22,9 @@ from keras_tools import extra_layers as XL
 from pylab import imshow
 
 frame_size = 128
-n_features = 4
-kern_len = 8
 shift = 8
 n_latent = 10
+noise_level = 0.01
 
 #act = lambda: L.LeakyReLU(alpha=0.3)
 use_bias = True
@@ -28,52 +35,11 @@ learning_rate = .1
 # loss_function = 'mean_squared_error'
 # loss_function = 'categorical_crossentropy'
 loss_function = lambda y_true, y_pred: \
-   keras.losses.mean_absolute_error(y_true, y_pred) \
+   keras.losses.mean_squared_error(y_true, y_pred) + keras.losses.mean_absolute_error(y_true, y_pred)
+   # keras.losses.mean_absolute_error(y_true, y_pred) \
    # + 0.1*keras.losses.mean_absolute_error(XL.power_spectrum(y_true), XL.power_spectrum(y_pred))
    # + keras.losses.mean_absolute_error(K.log(XL.power_spectrum(y_true)), K.log(XL.power_spectrum(y_pred)))
    # TODO lo/hipass: K.conv1d(x, kernel, strides=1, padding='valid', data_format=None, dilation_rate=1)
-
-
-def model_gen(x, reshape_in, reshape_out, conv, act, up):
-
-   enc1  = conv(2, kern_len, 1) >> act()
-   enc1b = conv(2, 1, 2) >> act()
-   enc2  = conv(4, kern_len, 1) >> act()
-   enc2b = conv(4, 1, 2) >> act()
-   enc3  = conv(8, kern_len, 1) >> act()
-   enc3b = conv(8, 1, 2) >> act()
-   enc4  = conv(16, kern_len, 1) >> act()
-   enc4b = conv(16, 1, 2) >> act()
-
-   #latent = conv(n_features, 2, 2, padding='valid') >> act()
-   to_latent = conv(n_features, 1, 2) >> act()
-   #to_latent = conv(16, 1, 2) >> act()
-   from_latent = up(2) >> conv(16, 1) >> act()
-
-   dec4  = up(2) >> conv(8, 1) >> act()
-   dec4b =          conv(8, kern_len) >> act()
-   dec3  = up(2) >> conv(4, 1) >> act()
-   dec3b =          conv(4, kern_len) >> act()
-   dec2  = up(2) >> conv(2, 1) >> act()
-   dec2b =          conv(2, kern_len) >> act()
-   dec1  = up(2) >> conv(1, 1) >> act()
-   dec1b =          conv(1, kern_len) >> act()
-
-   # ----- debug -----------------------------------
-   # m = M.Model([x], [(reshape_in >> enc1 >> enc1b >> enc2 >> enc2b >> enc3 >> enc4 >> to_latent >> dec4 >> dec3 >> dec2 >> dec2b >> dec1 >> dec1b >> reshape_out)(x)])
-   # for l in m.layers:
-   #    print(l.output_shape[1:])
-
-   y1 = reshape_in >> enc1 >> enc1b >> dec1 >> dec1b >> reshape_out
-   y2 = reshape_in >> enc1 >> enc1b >> enc2 >> enc2b >> dec2 >> dec2b >> dec1 >> dec1b >> reshape_out
-   y3 = reshape_in >> enc1 >> enc1b >> enc2 >> enc2b >> enc3 >> enc3b >> dec3 >> dec3b >> dec2 >> dec2b >> dec1 >> dec1b >> reshape_out
-   y4 = reshape_in >> enc1 >> enc1b >> enc2 >> enc2b >> enc3 >> enc3b >> enc4 >> enc4b >> dec4 >> dec4b >> dec3 >> dec3b >> dec2 >> dec2b >> dec1 >> dec1b >> reshape_out
-   y  = reshape_in >> enc1 >> enc1b >> enc2 >> enc2b >> enc3 >> enc3b >> enc4 >> enc4b >> to_latent >> from_latent >> dec4 >> dec4b >> dec3 >> dec3b >> dec2 >> dec2b >> dec1 >> dec1b >> reshape_out
-
-   model = M.Model([x], [y(x)])
-   joint_model = M.Model([x], [y1(x),y2(x),y3(x),y4(x),y(x)])
-
-   return model, joint_model
 
 
 
@@ -84,7 +50,8 @@ def print_layer_outputs(model):
 def make_signal(n_samples=10000):
    t = np.arange(n_samples)
    sin = np.sin
-   signal = 4. * sin(0.2*t + 6*sin(0.02*t))  +  5. * sin(t/3.7 + .3) \
+   signal = 4. * sin(0.2*t + 6*sin(0.02*t)) # +  5. * sin(t/3.7 + .3) \
+   # signal = 4. * sin(0.2*t + 6*sin(0.02*t) + 4*sin(0.043*t))
 
    #signal = 5. * sin(t/3.7 + .3) \
    #       + 3. * sin(t/1.3 + .1) \
@@ -102,11 +69,8 @@ def make_training_set(frame_size=30, n_pairs=1000, shift=1):
 
 in_frames, out_frames  = make_training_set(frame_size=frame_size, n_pairs=10000, shift=shift)
 
-# model, joint_model = make_model([sig_len], model_gen, use_bias=use_bias)
-# model.compile(optimizer=keras.optimizers.SGD(lr=learning_rate), loss=loss_function)
-# joint_model.compile(optimizer=keras.optimizers.SGD(lr=learning_rate), loss=loss_function)
-
 act = lambda: L.Activation('tanh')
+activation = fun.bind(XL.tanhx, alpha=0.1)
 #act = lambda: L.LeakyReLU(alpha=0.5)
 
 def dense(units, use_bias=True):
@@ -115,6 +79,7 @@ def dense(units, use_bias=True):
 
 def make_dense_model(sig_len, latent_size):
    x = input_like(in_frames[0])
+   #y = dense(sig_len/2) >> dense(sig_len)
    y = dense(sig_len/2) >> dense(latent_size) >> dense(sig_len/2) >> dense(sig_len)
    # y = dense(sig_len/2) >> dense(sig_len/4) >> dense(latent_size) >> dense(sig_len/4) >> dense(sig_len/2) >> dense(sig_len)
    return M.Model([x], [y(x)])
@@ -128,7 +93,6 @@ def train(model, inputs, target, batch_size, n_epochs, loss_recorder):
 
 
 model = make_dense_model(len(in_frames[0]), n_latent)
-#model, joint_model = make_model([len(in_frames[0])], model_gen, use_bias=use_bias)
 
 
 print('model shape')
@@ -140,16 +104,21 @@ loss_recorder = tools.LossRecorder()
 # model.save_weights('timeshift_autoencoder_dense_64_32_10.hdf5')
 # model.save_weights('timeshift_autoencoder_dense_64_32_10.hdf5')
 
+# M.save_model(model, "timeshift_autoencoder_dense_64_32_10__shift8__just_fm-mode_.hdf5")
+# model = M.load_model("timeshift_autoencoder_dense_64_32_10__shift8__just_fm-mode_.hdf5",
+#                      custom_objects={'<lambda>': loss_function})
+
+
 # joint_model does only work with real auto encoders
 
 model.compile(optimizer=keras.optimizers.SGD(lr=0.5), loss=loss_function)
-tools.train(model, in_frames, out_frames, 20, n_epochs, loss_recorder)
+tools.train(model, tools.add_noise(in_frames, noise_level), out_frames, 20, n_epochs, loss_recorder)
 
 model.compile(optimizer=keras.optimizers.SGD(lr=0.1), loss=loss_function)
-tools.train(model, in_frames, out_frames, 20, n_epochs, loss_recorder)
+tools.train(model, tools.add_noise(in_frames, noise_level), out_frames, 20, n_epochs, loss_recorder)
 
 model.compile(optimizer=keras.optimizers.SGD(lr=0.01), loss=loss_function)
-tools.train(model, in_frames, out_frames, 50, 2*n_epochs, loss_recorder)
+tools.train(model, tools.add_noise(in_frames, noise_level), out_frames, 50, 2*n_epochs, loss_recorder)
 
 from pylab import *
 
@@ -213,12 +182,15 @@ def predict_signal(n_samples):
       pred_sig = xfade_append(pred_sig, f, shift)
    return pred_sig
 
-sig = make_signal(60000)
-pred_sig = predict_signal(60000)
+def plot_prediction(n=2000):
+   sig = make_signal(n+100)
+   pred_sig = predict_signal(n+100)
+   fig, ax = pl.subplots(2,1)
+   ax[0].plot(sig[:n], 'k')
+   ax[0].plot(pred_sig[:n], 'r')
+   ax[1].plot(sig[:n]-pred_sig[:n])
 
-figure()
-plot(sig[:2000], 'k')
-plot(pred_sig[:2000], 'r')
+plot_prediction(3000)
 
 # import sounddevice as sd
 # sd.play(sig, 44100)
