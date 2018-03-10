35bbf0843fff150742448eb3aa50ffbb7a2d26a1
André Bergner <andre.bergner.0@gmail.com>
2018-03-10 22:07:27 +0100

    Adding 2D TAE playground.

---
+++ neuronal_net/timeshift_autoencoder/timeshift_autoencoder_playground_2d.py
@@ -0,0 +1,129 @@
+from imports import *
+from predictors import *
+#from test_signals import *
+import pylab as pl
+from pylab import *
+
+
+# -----------------------------------------------------------------------------------
+# MODEL PARAMETERS
+# -----------------------------------------------------------------------------------
+
+kern_len = 5
+noise_stddev = 0.03
+noise_level = 0.0#1
+use_bias = True
+n_nodes = 20
+n_latent = 20
+frame_size = 128
+shift = 16
+n_pairs = 1000
+n_epochs = 50
+noise_stddev = 0.01
+
+loss_function = lambda y_true, y_pred: \
+   keras.losses.mean_squared_error(y_true, y_pred) + keras.losses.mean_absolute_error(y_true, y_pred)
+   # keras.losses.mean_absolute_error(y_true, y_pred) \
+   # + 0.1*keras.losses.mean_absolute_error(XL.power_spectrum(y_true), XL.power_spectrum(y_pred))
+   # + keras.losses.mean_absolute_error(K.log(XL.power_spectrum(y_true)), K.log(XL.power_spectrum(y_pred)))
+
+activation = fun.bind(XL.tanhx, alpha=0.1)
+act = lambda: L.Activation(activation)
+dense = lambda s: F.dense(s, activation=None, use_bias=use_bias)
+conv1d = lambda feat: F.conv1d(int(feat), kern_len, stride=1, activation=None, use_bias=use_bias)
+
+
+# -----------------------------------------------------------------------------------
+# DATA GENERATION
+# -----------------------------------------------------------------------------------
+
+# customization wrapper for ginzburg-landau generator
+def ginz_lan(n):
+   x = TS.ginzburg_landau(n_samples=n, n_nodes=n_nodes, beta=0.1+0.5j)
+   #return abs(x[:,:,0] + 1j*x[:,:,1])
+   return x[:,:,0]
+
+make_signal = lambda n: ginz_lan(n)#[:,5]
+
+in_frames, out_frames, next_samples, next_samples2 = TS.make_training_set(make_signal, frame_size=frame_size, n_pairs=n_pairs, shift=shift, n_out=2)
+in_frames = in_frames.transpose(0,2,1)
+out_frames = [out_frames[0].transpose(0,2,1), out_frames[1].transpose(0,2,1)]
+
+
+
+def make_model_2d(example_frame, latent_size, simple=True):
+   sig_len = example_frame.shape[-1]
+   #x = F.noise(noise_stddev)(F.input_like(example_frame))
+   x = F.input_like(example_frame)
+   eta = F.noise(noise_stddev)
+
+   # tp = fun._ >> L.Permute((1, 2))  TODO: try using permute/transpose
+
+   if simple:
+
+      print("simple model")
+
+      enc1 = conv1d(sig_len/2) >> act() # >> F.dropout(0.2)
+      enc2 = conv1d(sig_len/4) >> act() # >> F.dropout(0.2)
+      #enc3 = conv1d(latent_size)
+      enc3 = F.flatten() >> dense([n_latent]) >> act() # >> F.batch_norm()
+
+      # TODO: figure out dimension from shape
+      dec3 = dense([n_nodes, int(sig_len/4)]) >> act() # >> F.batch_norm() >> F.dropout(0.2)
+      #dec3 = conv1d(sig_len/4)
+      dec2 = conv1d(sig_len/2) >> act() # >> F.dropout(0.2)
+      dec1 = conv1d(sig_len) >> act()
+
+   else:
+
+      enc1 = conv1d(sig_len/2) >> act() >> F.dropout(0.2)
+      enc2a = conv1d(sig_len/4) >> act() >> F.dropout(0.2)
+      enc2b = conv1d(sig_len/4) >> act() >> F.batch_norm() >> F.dropout(0.2)
+      enc2 = enc2a >> enc2b
+      enc3 = F.flatten() >> dense([n_latent]) >> act() >> F.batch_norm()
+
+      # TODO: figure out dimension from shape
+      dec3 = dense([n_nodes, int(sig_len/4)]) >> act() >> F.batch_norm() >> F.dropout(0.2)
+      dec2b = conv1d(sig_len/4) >> act() >> F.batch_norm() >> F.dropout(0.2)
+      dec2a = conv1d(sig_len/2) >> act() >> F.dropout(0.2)
+      dec2 = dec2b >> dec2a
+      dec1 = conv1d(sig_len) >> act()
+
+      # dec4 = up(2) >> conv(8, 1) >> act()
+
+   encoder = enc1 >> enc2 >> enc3
+   decoder = dec3 >> dec2 >> dec1
+   y = encoder >> decoder
+   latent = encoder(x)
+   out = decoder(latent)
+   return M.Model([x], [out]), M.Model([x], [out, y(out)]), M.Model([x], [latent])#, XL.jacobian(latent,x)
+
+
+
+
+# 2D conv-model
+model, model2, encoder = make_model_2d(in_frames[0], n_latent, simple=True)
+
+tools.print_layer_outputs(model)
+loss_recorder = tools.LossRecorder()
+
+model2.compile(optimizer=keras.optimizers.Adam(), loss=loss_function)
+tools.train(model2, tools.add_noise(in_frames, noise_level), out_frames, 32, n_epochs, loss_recorder)
+
+model.compile(optimizer=keras.optimizers.SGD(lr=0.01), loss=loss_function)
+
+
+figure()
+semilogy(loss_recorder.losses)
+
+
+def plot_prediction_im(n=2000, signal_gen=make_signal):
+   sig = signal_gen(n+100).T
+   pred_sig = predict_signal(model, sig[:,:frame_size], shift, n+100)
+   fig, ax = pl.subplots(3,1)
+   ax[0].imshow(log(.1 + abs(sig[:n])), aspect='auto')
+   ax[1].imshow(log(.1 + abs(pred_sig[:n])), aspect='auto')
+   ax[2].imshow(log(.1 + abs(sig[:,:n]-pred_sig[:,:n])), aspect='auto')
+
+
+plot_prediction_im(3000, make_signal)
