b3c0ba83251245aecc98b41c3b2ae912c24e1ec4
André Bergner <andre.bergner.0@gmail.com>
2018-03-20 21:04:00 +0100

    adding bootstrap script.

---
+++ neuronal_net/bootstrap.sh
@@ -0,0 +1,6 @@
+# brew upgrade ipython3
+pip3 install ipython
+pip3 install numpy scipy matplotlib
+pip3 install theano tensorflow keras h5py
+pip3 install pillow   # for loading images
+pip3 install sounddevice
