9ddb9b50dead54f4406685f2f2168a529b9285ed
André Bergner <andre.bergner.0@gmail.com>
2018-03-22 20:56:57 +0100

    Added sepcial layer LayerNorm1D from paper arxiv/1607.06450.

--- neuronal_net/keras_tools/extra_layers.py
+++ neuronal_net/keras_tools/extra_layers.py
@@ -191,3 +191,37 @@ keras.utils.generic_utils.get_custom_objects().update({
     'soft_relu': L.Activation(soft_relu),
     'tanhx': L.Activation(tanhx)
 })
+
+
+
+
+
+class LayerNorm1D(Layer):
+
+    # https://arxiv.org/pdf/1607.06450.pdf
+    # https://github.com/keras-team/keras/issues/3878
+
+    def __init__(self, eps=1e-6, **kwargs):
+        self.eps = eps
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.gamma = self.add_weight(name='gamma',
+                                     shape=input_shape[1:],
+                                     initializer=keras.initializers.Ones(),
+                                     trainable=True)
+
+        self.beta = self.add_weight(name='beta',
+                                    shape=input_shape[1:],
+                                    initializer=keras.initializers.Zeros(),
+                                    trainable=True,)
+
+        super().build(input_shape)
+
+    def call(self, x):
+        mean = K.mean(x, axis=-1, keepdims=True)
+        std = K.std(x, axis=-1, keepdims=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
