fdaa68f55d6e63944b64c1d7b7338a41e661dbc6
André Bergner <andre.bergner.0@gmail.com>
2018-03-22 20:55:59 +0100

    uddating booststrap script.

--- neuronal_net/bootstrap.sh
+++ neuronal_net/bootstrap.sh
@@ -2,5 +2,5 @@
 pip3 install ipython
 pip3 install numpy scipy matplotlib
 pip3 install theano tensorflow keras h5py
-pip3 install pillow   # for loading images
+pip3 install pillow scikit-image  # for loading and resizing images
 pip3 install sounddevice
