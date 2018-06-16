00901e7c1daf09987fcc5920900944c3a5dfabc4
André Bergner <andre.bergner.0@gmail.com>
2018-06-16 22:47:10 +0200

    SSAE: training cheater on full parallel AE.

--- neuronal_net/autoencoder/source_separation_autoencoder.py
+++ neuronal_net/autoencoder/source_separation_autoencoder.py
@@ -1,4 +1,8 @@
 # TRY
+# • minimize cross-channel predictivity, i.e. it should be impossible for the model to predict
+#   the x2 out z1 and vise versa
+#   • perhaps formulate in terms of ShAE, i.e. it's not possible to build a sub-ShAE across
+#     the channels
 # • simplify (remove layers) from decoder
 # • simplify parallel AE
 # • multi-pass ShAE
@@ -35,7 +39,7 @@ n_epochs = 100
 noise_stddev = 0.1
 
 
-activation = fun.bind(XL.tanhx, alpha=0.1)
+activation = fun.bind(XL.tanhx, alpha=0.2)
 act = lambda: L.Activation(activation)
 #act = lambda: L.LeakyReLU(alpha=0.2)
 
@@ -159,9 +163,17 @@ def make_model2(example_frame, latent_sizes=[n_latent1, n_latent2]):
    # y = eta() >> encoder >> eta() >> decoder
    y1 = eta() >> encoder1 >> eta() >> decoder1
    y2 = eta() >> encoder2 >> eta() >> decoder2
-   add = L.Add()
-   # latent = encoder(x)
-   return M.Model([x], [add([y1(x), y2(x)])]), M.Model([x], [y1(x)]), M.Model([x], [y2(x)])
+   y = lambda x: L.Add()([y1(x), y2(x)])
+
+   return (
+      None,#M.Model([x], [loss([x, y(x)])]),
+      M.Model([x], [y(x)]),
+      M.Model([x], [y(x), y(y(x))]),
+      M.Model([x], [y1(x)]),
+      M.Model([x], [y2(x)]),
+      M.Model([x], [encoder1(x)]),
+      M.Model([x], [encoder2(x)]),
+   )
 
 
 def windowed(xs, win_size):
@@ -192,16 +204,19 @@ fm_strong = lambda n: 0.5*np.sin(0.02*np.arange(n) + 4*np.sin(0.11*np.arange(n))
 fm_hyper = lambda n: np.sin(0.02*np.arange(n) + 4*np.sin(0.11*np.arange(n)) + 2*np.sin(0.009*np.arange(n)))
 lorenz = lambda n: TS.lorenz(n, [1,0,0])[::1]
 lorenz2 = lambda n: TS.lorenz(n+15000, [0,-1,0])[15000:]
-sig1 = lorenz
-sig2 = fm_strong
+sig1 = lambda n: 0.3*lorenz(n)
+sig2 = lambda n: 0.3*fm_strong(n)
 make_2freq = lambda n: sig1(n) + sig2(n)
 
 frames, out_frames, *_ = TS.make_training_set(make_2freq, frame_size=frame_size, n_pairs=n_pairs, shift=shift, n_out=2)
 
+frames1, *_ = TS.make_training_set(sig1, frame_size=frame_size, n_pairs=n_pairs, shift=shift, n_out=2)
+frames2, *_ = TS.make_training_set(sig2, frame_size=frame_size, n_pairs=n_pairs, shift=shift, n_out=2)
 
 
 
-trainer, model, model2, mode1, mode2, encoder, dzdx = make_model(frames[0])
+
+trainer, model, model2, mode1, mode2, encoder, dzdx = make_model2(frames[0])
 loss_function = lambda y_true, y_pred: keras.losses.mean_squared_error(y_true, y_pred) #+ 0.001*K.sum(dzdx*dzdx)
 model.compile(optimizer=keras.optimizers.Adam(), loss=loss_function)
 model.summary()
@@ -224,7 +239,7 @@ loss_recorder = tools.LossRecorder()
 #tools.train(model, frames, out_frames[0], 32, n_epochs, loss_recorder)
 #tools.train(model, frames, out_frames[0], 128, 15*n_epochs, loss_recorder)
 tools.train(cheater, frames, [frames1, frames2], 32, n_epochs, loss_recorder)
-tools.train(model, frames, frames, 32, n_epochs, loss_recorder)
+#tools.train(model, frames, frames, 32, n_epochs, loss_recorder)
 #tools.train(model, frames, frames, 128, 15*n_epochs, loss_recorder)
 #tools.train(trainer, frames, frames, 32, n_epochs, loss_recorder)
 #tools.train(mode1, frames, frames, 32, n_epochs, loss_recorder)
