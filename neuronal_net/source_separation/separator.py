ab8e4c749cbdc991a3b99ae5e0759a0b4ff77e5e
André Bergner <andre.bergner.0@gmail.com>
2018-10-13 09:29:18 +0200

    separator: small clean up.

--- neuronal_net/source_separation/separator.py
+++ neuronal_net/source_separation/separator.py
@@ -5,30 +5,33 @@ from keras_tools import test_signals as TS
 
 class Separator:
 
-   def __init__(self, signal, frame_size, latent_sizes):
+   def __init__(self
+   ,  signal, frame_size
+   ,  latent_sizes
+   ,  noise_stddev=2.0
+   ,  noise_decay=0.002
+   ,  factory=ConvFactory  # DenseFactory, DilatedFactory
+   ,  verbose=False
+   ):
 
       assert(type(frame_size) == int)
       assert(type(latent_sizes) == list or type(latent_sizes) == tuple)
 
-      frame_size0 = 128
-      factor = frame_size // frame_size0
-
-      noise_stddev = 2.0
-      noise_decay = 0.002
+      FRAME_SIZE0 = 128
+      factor = frame_size // FRAME_SIZE0
 
       self.frames = np.array([w for w in windowed(signal, frame_size, 1)])
 
-      # factory = DenseFactory
-      factory = ConvFactory
-      # factory = DilatedFactory
       self.model, self.encoder, _, self.modes = make_factor_model(
          self.frames[0], factory(self.frames[0], latent_sizes, use_batch_norm=False, scale_factor=factor),
          noise_stddev=noise_stddev, noise_decay=noise_decay, shared_encoder=True
       )
 
       self.model.compile(optimizer=keras.optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, decay=0.0001), loss='mse')
-      # self.model.summary()
-      # plot_model(self.model, to_file='ssae.png', show_shapes=True)
+
+      if verbose:
+         self.model.summary()
+         plot_model(self.model, to_file='ssae.png', show_shapes=True)
 
       self.loss_recorder = LossRecorder(lambda n,m: pred_error(self.modes[n], self.frames, [sig1,sig2][m], 2048))
 
@@ -47,7 +50,8 @@ def separate(signal, frame_size, latent_sizes):
    return separator
 
 
-sig1, sig2 = lorenz, 0.8*fm_strong
+#sig1, sig2 = lorenz, 0.8*fm_strong
+sig1, sig2 = lorenz, 0.5*fm_strong0
 sig_gen = sig1 + sig2
 signal = sig_gen(5000)
 
